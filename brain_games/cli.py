8215a3942b352c64379ff1d1fe043284ccc44f50
EAMR <eamorales482@gmail.com>
2026-06-04 16:17:46 -0500

    fix en cli.py mayuscula y espacio

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,9 @@
 
 def welcome_user():
     #name = prompt.string('May I have yor name?') No me funciono de esta manera...
-    name = input('May I have your name? ')
+    name = input('May I have your name? ').strip
     while name.strip() == '':
-        name = input('May I have your name? ')
+        name = input('May I have your name? ').strip()
     
-    print(f'hello, {name}!')
+    print(f'Hello, {name}!')
     return name
