024a0616ee88fae806392a6e99353ed34bff8aa6
EAMR <eamorales482@gmail.com>
2026-06-04 16:28:11 -0500

    fix en cli.py quita strip()

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,7 +2,7 @@
 def welcome_user():
     #name = prompt.string('May I have yor name?') No me funciono de esta manera...
     name = input('May I have your name? ').strip
-    while name.strip() == '':
+    while name == '':
         name = input('May I have your name? ').strip()
     
     print(f'Hello, {name}!')
